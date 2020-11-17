cf344f1ccd7c63c6b8eff16802f8f372f152f1f6
urbloodynightmare <71308814+urbloodynightmare@users.noreply.github.com>
2020-11-17 12:12:14 +0300

    Homework2

--- hw2.py
+++ hw2.py
@@ -1,20 +1,23 @@
 import math
-def quadratic_equation(a,b,c):
-    d = b*b - 4*a*c
+def calc_quadration_equation(a,b,c):
+    def calc_discriminant(a,b,c):
+        d = b*b - 4*a*c
+        return d
+    d=calc_discriminant(a,b,c)
     if d > 0:
         return (-b+math.sqrt(d)) / (2*a), (-b-math.sqrt(d)) / (2*a)
     if d == 0:
         return (-b+math.sqrt(d)) / (2*a)
     else:
-        return 'Нет корней'
+        return 'no roots'
 
 
-def linear_equation(b,c):
+def calc_linear_equation(b,c):
     return -c/b
 
 
 a, b, c = map(int,input('Введитте a, b и c: ').split())
 if a == 0:
-    print(linear_equation(b,c))
+    print(calc_linear_equation(b,c))
 else:
-    print(quadratic_equation(a,b,c))
+    print(calc_quadration_equation(a,b,c))
