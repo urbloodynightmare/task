0d04fb244f4fe9b5b542e09f015e0318464293f6
urbloodynightmare <71308814+urbloodynightmare@users.noreply.github.com>
2020-11-17 11:47:43 +0300

    Homework

--- hw2.py
+++ hw2.py
@@ -2,9 +2,9 @@ import math
 def quadratic_equation(a,b,c):
     d = b*b - 4*a*c
     if d > 0:
-        return (-b+math.sqrt(d))/(2*a), (-b-math.sqrt(d))/(2*a)
-    if d==0:
-        return (-b+math.sqrt(d))/(2*a)
+        return (-b+math.sqrt(d)) / (2*a), (-b-math.sqrt(d)) / (2*a)
+    if d == 0:
+        return (-b+math.sqrt(d)) / (2*a)
     else:
         return 'Нет корней'
 
@@ -14,7 +14,7 @@ def linear_equation(b,c):
 
 
 a,b,c = map(int,input().split())
-if a==0:
+if a == 0:
     print(linear_equation(b,c))
 else:
     print(quadratic_equation(a,b,c))
